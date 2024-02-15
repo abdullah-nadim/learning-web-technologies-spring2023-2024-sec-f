f7da646e09fe80858b499f5fe91985fd308f9fc9
abdullah-nadim <nadim.soft.111@gmail.com>
2024-02-15 22:49:05 +0600

    made changes to lab task 8

--- Lab Task/lab 8.php
+++ Lab Task/lab 8.php
@@ -1,13 +1,42 @@
 <?php
-echo "<table cellpadding='0' cellspacing='0' border='1' align='center' width='50%' height='00px'>";
+echo "<table cellpadding='0' cellspacing='0' border='1' align='center' width='50%' height='100px'>";
+echo "<tr>
+                <td align='center' rowspan='1' colspan='2'>
+                    The array to declare
+                </td>
+                <td align='center' rowspan='1' colspan='2'>
+                    Shapes to print
+                </td>     
+     </tr>";
 echo "<tr>";
+
+echo "<td rowspan='1' colspan='2'>";
+echo "<table cellpadding='0' cellspacing='0' border='1' align='center' width='80%' height='20px' style='background-color: #f2f2f2;'>";
+
     $arr = [
             [1,2,3,'A'],
             [1,2,'B','C'],
             [1,'D','E','F'],
         ];
+    foreach( $arr as $i)
+    {
+        echo "<tr align='center'>";
+        foreach($i as $j)
+        {
+            echo "<td>";
+            echo $j;
+            echo "</td>";
+        }
+        echo "</tr>";
+
+    }
+echo"</table>";
+echo "</td>";
 
 echo "<td>";           
+echo "<table cellpadding='0' cellspacing='0' border='1' align='center' width='80%' height='20px' style='background-color: #f2f2f2;'>";
+echo "<tr>";
+echo "<td>";
 for($i=0;$i<3;$i+=1)
     {   
         for($j=0;$j<(3-$i);$j+=1)
@@ -27,5 +56,9 @@ for($i=0;$i<3;$i+=1)
         echo "<br>";
     }
 echo "</td>";
+echo "</tr>";
+echo"</table>";
+echo "</tr>";
+echo "</table>"
 ?>
 
